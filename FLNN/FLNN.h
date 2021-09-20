cbf82823c23ea4cbaceba5d1d2ad6a2e7b586d95
Ria9993 <hyunjun2372@gmail.com>
2021-09-20 11:33:00 +0900

    commit

--- FLNN/FLNN.h
+++ FLNN/FLNN.h
@@ -6,22 +6,39 @@
 using namespace std;
 
 class FLNN {
-public :
+private :
+
+public:
 	int layer_num_;
 	int layer_node_num_;
 	int input_node_num_;
 	int output_node_num_;
-	
+
+	//HyperParametor
 	double learning_rate_;
 	double clipping_threshold_;
 
 	void make();
 	void forward_step();
 	void optimize();
-private :
-	
+
 	FLNN() {
 		learning_rate_ = 0.001f;
 		clipping_threshold_ = 100.0f;
 	}
 };
+
+inline void FLNN::make() {
+	
+	return;
+}
+
+inline void FLNN::forward_step()
+{
+
+}
+
+inline void FLNN::optimize()
+{
+
+}
