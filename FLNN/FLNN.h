d8a1df3bcbe9d24a00e2b8e96eccc40ec5141a65
Ria9993 <hyunjun2372@gmail.com>
2021-09-20 13:05:49 +0900

    commit

--- FLNN/FLNN.h
+++ FLNN/FLNN.h
@@ -12,12 +12,14 @@ public:
 	//Parametor
 	int node_num_;
 	string activation_;
-	vector<Layer*> next_; ///< pointer next layer (can multiple)
+	vector<Layer*> next_; ///< Pointer next layer (can multiple)
 	vector<vector<double>> weight_;
+	vector<vector<double>> stochastic_gate_rate_;
 	vector<double> bias_;
 	
 	//Backprop
 	vector<vector<double>> weight_grad_;
+	vector<vector<int>> stochastic_gate_grad_;
 	vector<double> grad_;
 
 	//Function
@@ -37,7 +39,7 @@ public:
 	//optional
 	double learning_rate_;
 	double clipping_threshold_;
-	double backprop_truncate_step_; ///< backprop 반복 최대 횟수 설정
+	double backprop_truncate_step_; ///< Backprop 반복 최대 횟수 설정
 	string activation_;
 	string loss_;
 	double stochastic_rate_; ///< 신경 전달 확률
@@ -45,7 +47,7 @@ public:
 	//Function
 	void make(); ///< Build & make the model
 	void forward_step(vector<double> input); ///< 한 전체 루프를 진행
-	void forward_step();
+	void forward_step(); ///< 없는 input으로 loop된 데이터만 공회전
 	void optimize(vector<double> target);
 	void reset();
 
@@ -74,14 +76,19 @@ inline void FLNN::forward_step(vector<double> input)
 
 inline void FLNN::forward_step()
 {
+	vector<double> empty_input;
+	empty_input.resize(input_node_num_);
+	memset(empty_input, 0, sizeof(empty_input));
 
+	forward_step(empty_input)
+
+	return;
+}
+
+inline void FLNN::optimize(vector<double> target)
+{
 }
 
 inline void FLNN::reset()
 {
-}
-
-inline void FLNN::optimize()
-{
-
 }
